1299d0de87ae68183f1057de4f7ac518a3e1bf7c
Максим Шабанов <maksim20048.ru@gmail.com>
2023-02-10 20:29:34 +0400

    initial commit

--- 7.cs
+++ 7.cs
@@ -1,1 +1,45 @@
+using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
 
+string pathToFile = "D:\\TestFile.txt";
+string[] linesOfText = File.ReadAllLines(pathToFile);
+Dictionary<string, List<List<string>>> cookBook = new Dictionary<string, List<List<string>>>();
+addFileToCookBook(linesOfText, cookBook);
+void addFileToCookBook(string[] linesOfText, Dictionary<string, List<List<string>>> cookBook)
+{
+    int offset = 0, totalOffset = 0;
+    bool endOfText = false;
+    while (!endOfText)
+    {
+        string dishName = linesOfText[0 + offset];
+        List<List<string>> composition = new List<List<string>>();
+        for (int i = 0; i < Convert.ToInt32(linesOfText[1 + offset]); i++)
+        {
+            List<string> ingridient = new List<string>();
+
+            ingridient.Add("ingredientName");
+            int iOFS = linesOfText[2 + offset + i].IndexOf('|');                //indexOfFirstSeparator 
+            ingridient.Add(linesOfText[2 + offset + i].Substring(0, iOFS - 1));
+
+            ingridient.Add("quantity");
+            int iOSS = linesOfText[2 + offset + i].IndexOf('|', iOFS + 1);      //indexOfSecondSeparator
+            ingridient.Add(linesOfText[2 + offset + i].Substring(iOFS + 2, iOSS - iOFS - 3));
+
+            ingridient.Add("measure");
+            ingridient.Add(linesOfText[2 + offset + i].Substring(iOSS + 2, linesOfText[2 + offset + i].Length - iOSS - 2));
+
+            totalOffset++;
+            composition.Add(ingridient);
+        }
+        totalOffset += 2;
+        if (cookBook.ContainsKey(dishName))
+            if (totalOffset == linesOfText.Length)
+                endOfText = true;
+        totalOffset += 1;
+        offset = totalOffset;
+
+    }
+}
